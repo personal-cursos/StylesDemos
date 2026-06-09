0714c0680fed4ced11b97d1d41b4e13181c5b30e
pavachoquegamer <pavachoquegamer@gmail.com>
2026-06-08 23:46:10 -0500

    stilos dinamicos

---
+++ StylesDemos/DynamicStylesView.xaml.cs
@@ -0,0 +1,19 @@
+namespace StylesDemos;
+
+public partial class DynamicStylesView : ContentPage
+{
+	public DynamicStylesView()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Application.Current
+			.Resources.TryGetValue("specialButton",
+			out var retVal);
+
+		Resources["dynamicStyle"] =
+			(Style) retVal;
+    }
+}
